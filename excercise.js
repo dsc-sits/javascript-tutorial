659cd2fcbfaa7f655e33f34ea346dfe7569cf554
Abhishek Patnaik <abhishekpatnaik77@gmail.com>
2019-02-28 20:37:24 +0530

    Add files via upload

---
+++ excercise.js
@@ -0,0 +1,46 @@
+// Display the sum of 5 + 10, using two variables: x and y.
+
+
+// var x = 5;
+// var y=10;
+// document.getElementById('dmeo').innerHTML = x+y;
+
+// 
+// firstName = "John"
+// lastName = "Doe"
+// age="35"
+//  
+// x=2
+// y=3
+// x+y 
+// x*3
+//
+// alert(10+5);
+// alert("Not allowed to view this page!!")
+//   
+// function myFunction(){
+// alert("Hello World !!");
+//  }
+// 
+//
+//
+// var person={
+// firstName:"John",   
+// lastName:"Doe" 
+// }
+// alert(person.firstName);
+// 
+//
+// var argument1 = {
+//    firstName:"John",
+//    mymethod : function(name) { 
+//    this.firstName=name; }
+// 
+//   }; 
+// 
+// argument1.firstName
+// argument1.mymethod("Julia")
+// argument1.firstName
+//
+// 
+//
